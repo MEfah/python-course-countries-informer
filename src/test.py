cd4d7593e80c278d259a89921ddf3e2a3f18df73
MEfah <msefanovv@gmail.com>
2024-03-12 21:58:12 +0500

    Testing currency

---
+++ src/test.py
@@ -0,0 +1,64 @@
+from http import HTTPStatus
+from typing import Optional
+
+import httpx
+
+from app.settings import REQUESTS_TIMEOUT
+from base.clients.base import BaseClient
+from geo.clients.shemas import CurrencyRatesDTO
+
+class CurrencyClient(BaseClient):
+    """
+    Реализация функций для взаимодействия с внешним сервисом-провайдером данных о курсах валют
+    """
+
+    def get_base_url(self) -> str:
+        return "https://www.cbr-xml-daily.ru/latest.js"
+
+    def _request(self, endpoint: str) -> Optional[dict]:
+        with httpx.Client(timeout=REQUESTS_TIMEOUT) as client:
+            # получение ответа
+            response = client.get(endpoint)
+            if response.status_code == HTTPStatus.OK:
+                return response.json()
+
+            return None
+
+    def get_currency_rates(self, currency: str) -> Optional[dict]:
+        """
+        Получение данных о курсах валют.
+
+        :param name: Название страны
+        :return:
+        """
+        
+        if response := self._request(self.get_base_url()):
+            item = response
+            rates = item["rates"]
+            currency = currency.upper()
+
+            if currency == "RUB":
+                return CurrencyRatesDTO(
+                    base=currency,
+                    date=item["date"],
+                    rates=rates
+                )
+            
+            elif currency in rates:
+                rate = rates[currency]
+                rates = { c: v / rate for c, v in rates.items() }
+                rates["RUB"] = 1 / rate
+                rates.pop(currency)
+                
+                ratesDTO = CurrencyRatesDTO(
+                    base=currency,
+                    date=item["date"],
+                    rates=rates
+                )
+                
+                return ratesDTO
+
+        return None
+    
+    
+print(CurrencyClient().get_currency_rates("RUB"))
